c64a29ca0f45ca8e9106fe5dfd81adfa5ad846b2
Alex045jeweetzelf <113170875+Alex045jeweetzelf@users.noreply.github.com>
2024-10-18 11:46:27 +0200

    location added

--- Victuz/Models/Location.cs
+++ Victuz/Models/Location.cs
@@ -5,17 +5,17 @@ namespace Victuz.Models
     public class Location
     {
         public int Id { get; set; }
-        [Required]
-        public string Name { get; set; }
-        [Required]
-        public string Street { get; set; }
-        [Required]
-        public int HouseNumber { get; set; }
-        [Required]
-        public string City { get; set; }
-        [Required]
-        public int MaxCapacity { get; set; }
-        [Required]
-        public ICollection<ActivityModel> Activities { get; set; }
+        //[Required]
+        public string? Name { get; set; }
+        //[Required]
+        public string? Street { get; set; }
+        //[Required]
+        public int? HouseNumber { get; set; }
+        //[Required]
+        public string? City { get; set; }
+        //[Required]
+        public int? MaxCapacity { get; set; }
+       // [Required]
+        public ICollection<ActivityModel>? Activities { get; set; }
     }
 }
