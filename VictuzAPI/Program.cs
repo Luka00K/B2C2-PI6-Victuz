2647acf3949e7b6e3d9b1161204eff1ee1307e6f
al3n5s <2065272reinfelds@zuyd.nl>
2024-11-01 00:20:54 +0100

    api crud voor activiteiten

--- VictuzAPI/Program.cs
+++ VictuzAPI/Program.cs
@@ -1,3 +1,4 @@
+using Victuz.Data;
 
 namespace VictuzAPI
 {
@@ -7,9 +8,18 @@ namespace VictuzAPI
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            builder.Services.AddDbContext<ApplicationDbContext>();
+
             // Add services to the container.
 
-            builder.Services.AddControllers();
+            builder.Services.AddControllers()
+                .AddJsonOptions(options =>
+                {
+                    options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.Preserve;
+                    options.JsonSerializerOptions.DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull;
+
+                });
+
             builder.Services.AddControllersWithViews();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
