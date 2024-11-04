8a798653bec2b027a789a9c00e8521192b972895
Robin Buijsen <2105346buijsen@zuyd.nl>
2024-11-04 13:31:53 +0100

    Voorstel Indienen

--- Victuz/Models/Suggestion.cs
+++ Victuz/Models/Suggestion.cs
@@ -5,9 +5,14 @@ namespace Victuz.Models
     public class Suggestion
     {
         public int Id { get; set; }
+
+        [Required]
+        public string? Title { get; set; }  
+
         [Required]
         public string? Description { get; set; }
-        [Required]
+
+        //[Required]
         public Member? Member { get; set; }
     }
 }
