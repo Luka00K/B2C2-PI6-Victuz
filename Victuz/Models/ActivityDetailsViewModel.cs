afa901a5bce2acdc75ec1c6ed1d06e58366fedc6
Luka00K <2103908komlenovic@zuyd.nl>
2024-11-05 12:18:16 +0100

    Registreren Aanwezigheid fix

--- Victuz/Models/ActivityDetailsViewModel.cs
+++ Victuz/Models/ActivityDetailsViewModel.cs
@@ -5,5 +5,7 @@
         public ActivityModel? Activity { get; set; }
         public bool IsRegistered { get; set; }
         public int AvailableSpots => (Activity.MaxParticipants ) - (Activity.Registrations?.Count ?? 0);
+
+        public int ActivityId => Activity?.Id ?? 0;
     }
 }
